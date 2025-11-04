bd77db05f5913b0a2fd06255cb5715d4b99a4c3d
SangWon7242 <gotkdqja@gmail.com>
2025-11-04 21:23:23 +0900

    프, 서버 데이터 조회, 코스 충돌로 실패

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,26 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+interface ChatRoom {
+  id: number;
+  createdDate: Date;
+  updateDate: Date;
+  name: string;
+}
+
 export default function App() {
-  return <div>안녕</div>;
+  const [chatRooms, setChatRooms] = useState<ChatRoom[]>([]);
+
+  useEffect(() => {
+    async function fetchChatRooms() {
+      const response = await fetch("http://localhost:8080/api/v1/chat/room");
+      const data = await response.json();
+      setChatRooms(data);
+    }
+
+    fetchChatRooms();
+  }, []);
+
+  return <div>{JSON.stringify(chatRooms)}</div>;
 }
