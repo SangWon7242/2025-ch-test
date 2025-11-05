539d445cd5fd11a95afd5bc592d77511df18a806
SangWon7242 <gotkdqja@gmail.com>
2025-11-05 19:51:55 +0900

    단건 조회

--- frontend/src/app/chat/rooms/[id]/page.tsx
+++ frontend/src/app/chat/rooms/[id]/page.tsx
@@ -1,3 +1,36 @@
+"use client";
+
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
+interface ChatRoom {
+  id: number;
+  createdDate: Date;
+  updateDate: Date;
+  name: string;
+}
+
 export default function ChatRoom() {
-  return <div>ChatRoom</div>;
+  const [chatRoom, setChatRoom] = useState<ChatRoom | null>(null);
+  const { id } = useParams();
+
+  const fetchChatRoom = async (id: string) => {
+    const response = await fetch(
+      `http://localhost:8080/api/v1/chat/rooms/${id}`
+    );
+    const data = await response.json();
+    setChatRoom(data);
+  };
+
+  useEffect(() => {
+    fetchChatRoom(id as string);
+  }, [id]);
+
+  return (
+    <div>
+      <h1>ChatRoom {chatRoom?.id}</h1>
+      <p>{chatRoom?.name}</p>
+      <p>{chatRoom?.createdDate?.toDateString()}</p>
+    </div>
+  );
 }
